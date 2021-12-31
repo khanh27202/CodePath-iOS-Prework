e093b25eeb2ac3aba6aefe835ee14d1b8ca8145c
khanh27202 <73024784+khanh27202@users.noreply.github.com>
2021-12-31 16:19:00 -0500

    Update Main.storyboard

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.orange
+    }
+    
 }
 
